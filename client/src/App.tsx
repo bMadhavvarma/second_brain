729897ff9e592156c20e0d1a931841b48e21a086
Madhav <varmamadhav71@gmail.com>
2025-08-16 12:54:37 +0530

    all set  for deploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import SignupForm from "./pages/SignupForm";
 import SigninForm from "./pages/SigninForm";
-import PublicBrain from './pages/PublicBrain.tsx';
-
-
+import PublicBrain from "./pages/PublicBrain";
 
 const App = () => {
   return (
-   <Routes>
-        <Route path="/signup" element={<SignupForm />} />
-        <Route path="/signin" element={<SigninForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/brain/:shareLink" element={<PublicBrain />} />
-
-   </Routes>
+    <Routes>
+      <Route path="/" element={<Navigate to="/signin" replace />} />
+      <Route path="/signup" element={<SignupForm />} />
+      <Route path="/signin" element={<SigninForm />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/brain/:shareLink" element={<PublicBrain />} />
+      {/* catch-all route for unknown paths */}
+      <Route path="*" element={<Navigate to="/signin" replace />} />
+    </Routes>
   );
 };
 
